6065dacff407157e0338aa768a9c63be39b83174
kevinmatthe <he_yuheng@163.com>
2026-03-09 14:42:39 +0800

    update

--- cmd/larkrobot/main.go
+++ cmd/larkrobot/main.go
@@ -24,7 +24,6 @@ import (
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
 	larkws "github.com/larksuite/oapi-sdk-go/v3/ws"
-	"gorm.io/gorm"
 )
 
 func main() {
